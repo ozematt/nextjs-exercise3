6190c4c9c80513c6072982e7786441033e4a79e4
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-27 20:08:52 +0100

    deleted logic

--- components/Main.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default function Main(props: any) {
-  const { children } = props;
-  return <main className="flex-1 flex flex-col p-4 sm:p-8">{children}</main>;
-}
